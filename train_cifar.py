d1d59daa532af820bd10ff9fee4ca340d8d6a620
dennis910130 <sic046@ucsd.edu>
2014-05-13 16:06:48 -0700

    modified:   train_cifar.py

--- train_cifar.py
+++ train_cifar.py
@@ -12,8 +12,8 @@ from layers import LogisticRegression,HiddenLayer,LeNetConvPoolLayer
 from myUtils import load_cifar_data
 import optparse
 
-def train_cifar(batch_size=128,n_epochs=200,test_frequency=1300,learning_rate_base=1, check_point_frequency=5000,show_progress_frequency=100):
-    check_point_path = '/home/chensi/mylocal/sichen/data/check_points/'
+def train_cifar(learning_rate_base=1,batch_size=128,n_epochs=20,test_frequency=1300, check_point_frequency=5000,show_progress_frequency=100):
+    check_point_path = os.environ['CHECK_POINT_PATH']
     parser = optparse.OptionParser()
     parser.add_option("-f",dest="filename", default='None')
 
